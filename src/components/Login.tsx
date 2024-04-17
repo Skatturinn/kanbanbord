51c0139aba605fe157bf9639a2a892ae325a132a
AddiSim <addisim12@gmail.com>
2024-04-17 17:44:58 +0000

    login component byrjun

---
+++ src/components/Login.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        try {
+            await axios.post('/login', { username, password });
+            console.log('Logged in')
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
