811008546a1833e4f4bb7c23edc0d8c53d819417
Elías <elias.ludviksson@gmail.com>
2024-04-18 17:24:25 +0000

    Komið addi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
-        
+         <h1>Þetta er heimasíðan</h1>
+		 <p>Hún er opin öllum, til að nota kanbanborð þarftu að skrá þig inn</p>
+		 <Link href="/Notandi/Login">Login</Link>
     </main>
   );
 }
