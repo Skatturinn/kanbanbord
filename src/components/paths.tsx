778a9d1be1a05e7fe19f180f13de1fb88ce765c2
Elías <elias.ludviksson@gmail.com>
2024-04-24 17:36:54 +0000

    index test

--- src/components/paths.tsx
+++ src/components/paths.tsx
@@ -20,7 +20,7 @@ export default function Paths({ files, image }: { files: Array<string | null>, i
 		ind[0] !== index && setIndex(ind[0])
 		test = false
 	}
-	console.log(pathname, p)
+	console.log(pathname, p, ind)
 	const active = p.map(stak => stak && isCurrent(stak[4]));
 	const stada = active.some(stak => stak);
 	return <nav>
