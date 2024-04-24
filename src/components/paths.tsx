3b818628c37fe1f44bff9e3f7844a6cccd384c3d
Elías <elias.ludviksson@gmail.com>
2024-04-24 17:31:50 +0000

    laga paths villu

--- src/components/paths.tsx
+++ src/components/paths.tsx
@@ -4,14 +4,22 @@ import { usePathname } from "next/navigation";
 import { PathString } from "react-hook-form";
 import Image from "next/image";
 import styles from "./Paths.module.scss"
+import { useState } from "react";
 
+let test = true;
 export default function Paths({ files, image }: { files: Array<string | null>, image: PathString | undefined }) {
 	const pathname = usePathname();
+	const [index, setIndex] = useState(2)
 	const isCurrent = (href: string): string => {
 		return pathname && pathname.split('/').includes(href) ? 'active' : 'ekki';
 	}
 	const ff = files.filter(e => e);
 	const p = ff[0]?.includes('\\') ? ff.map(stak => stak?.split('\\')) : ff.map(stak => stak?.split('/'));
+	const ind = p.map(stak => stak?.indexOf(pathname.split('/')[1])).filter(v => Number(v) >= 0) as Array<number>
+	if (test) {
+		ind[0] !== index && setIndex(ind[0] || 2)
+		test = false
+	}
 	const active = p.map(stak => stak && isCurrent(stak[4]));
 	const stada = active.some(stak => stak);
 	return <nav>
@@ -23,7 +31,7 @@ export default function Paths({ files, image }: { files: Array<string | null>, i
 			</li>
 			{p.map(
 				(stak, nr) => {
-					const href = stak && stak.splice(stada ? -1 : 2).join('/')
+					const href = stak && stak.splice(stada ? -1 : index).join('/')
 					return (href && !href.includes('api') &&
 						<li key={nr} className={isCurrent(href)}>
 							<Link href={'/' + href}>
